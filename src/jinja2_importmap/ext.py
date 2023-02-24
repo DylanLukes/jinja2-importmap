d58333122fdc267dfe7d23a679f7d24e8fcf6844
Dylan Lukes <lukes.dylan@gmail.com>
2023-02-24 02:16:02 -0800

    fix bug in ImportMapExtension

--- src/jinja2_importmap/ext.py
+++ src/jinja2_importmap/ext.py
@@ -10,9 +10,9 @@ from .core import scan_packages
 
 class ImportMapExtension(Extension):
 
-    def __init__(self, env: Environment):
-        super().__init__(env)
-        self.env.globals["importmap"] = scan_packages
+    def __init__(self, environment: Environment):
+        super().__init__(environment)
+        environment.globals["importmap"] = scan_packages
 
     def parse(self, parser: Parser) -> Union[Node, List[Node]]:
         raise NotImplementedError
